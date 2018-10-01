a297da81319ee5c22d20040eabda617819d83dee
Megan Hainline <meg.hainline@gmail.com>
2018-09-30 21:10:06 -0500

    change

--- CG 11-3/Program.cs
+++ CG 11-3/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace CG_11_3
+
+namespace StudentMinMax
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader file = new StreamReader("C:\\Users\\megha\\source\\repos\\CG 11-3\\CG 11-3\\studentNames.txt");
+
+            List<Student> students = new List<Student>();
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split(" ");
+                Student s = new Student(items[0]);
+
+                for (int i = 1; i < items.Length; i++)
+                {
+                    s.AddScore(Int32.Parse(items[i]));
+                }
+
+                students.Add(s);
+
+            }
+            foreach (Student item in students)
+            {
+
+                Console.WriteLine(item.Name + " " + item.MinGrade() + " " + item.MaxGrade());
+
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ CG 11-3/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace StudentMinMax
+{
+    class Student
+    {
+        public List<int> scores = new List<int>();
+        public string Name { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public void AddScore(int score)
+        {
+            scores.Add(score);
+        }
+
+        public int MinGrade()
+        {
+            return scores.Min();
+        }
+
+        public int MaxGrade()
+        {
+            return scores.Max();
+            {
+            }
+        }
+    }
+} 
